58440c22d3f7bd90d13f7d3eb377324aa2adf4fc
Tony <nya.tony2010@gmail.com>
2023-12-16 20:28:20 +0100

    feat: add routing between different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { EshopComponent } from './components/eshop/eshop.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'eshop', component: EshopComponent },
+  { path: 'account', component: MyAccountComponent },
+  { path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
