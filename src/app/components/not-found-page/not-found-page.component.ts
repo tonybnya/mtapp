635f913212658983e9b8c6da8ce3ef00ffa9f011
Tony <nya.tony2010@gmail.com>
2023-12-16 23:51:26 +0100

    feat: design of the not-found-page component with redirection buttons

--- src/app/components/not-found-page/not-found-page.component.ts
+++ src/app/components/not-found-page/not-found-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.scss']
 })
-export class NotFoundPageComponent {
+export class NotFoundPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+  }
+
+  onRedirectHome() {
+    this.router.navigateByUrl('home');
+  }
+
+  onRedirectEshop() {
+    this.router.navigateByUrl('eshop');
+  }
 }
