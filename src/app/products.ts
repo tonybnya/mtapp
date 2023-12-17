534330ca61ec20ba4e69e636fb8c44b56fe7bfa1
Tony <nya.tony2010@gmail.com>
2023-12-17 18:33:53 +0100

    style: refactor, add quantity attribute for products, and change url attribute to imgURL

--- src/app/products.ts
+++ src/app/products.ts
@@ -11,119 +11,134 @@ export const products = [
     name: 'Laptop Protective Bag',
     price: 21,
     description: 'A protective laptop bag with padded design for secure and stylish transport.',
-    url: 'https://i.postimg.cc/rp8FK5mS/laptop-protective-bag.jpg',
-    category: 'Accessory'
+    imgURL: 'https://i.postimg.cc/rp8FK5mS/laptop-protective-bag.jpg',
+    category: 'Accessory',
+    quantity: 10
   },
   {
     id: 2,
     name: 'SAMSUNG Galaxy A54',
     price: 349,
     description: 'A Sleek and brilliant smartphone with great display, powerful camera, enduring battery.',
-    url: 'https://i.postimg.cc/mZmswhwS/samsung-galaxy-a5.jpg',
-    category: 'Phone'
+    imgURL: 'https://i.postimg.cc/mZmswhwS/samsung-galaxy-a5.jpg',
+    category: 'Phone',
+    quantity: 23
   },
   {
     id: 3,
     name: 'LG Ultra 16 U7R Laptop',
     price: 629,
     description: 'A powerful computer combining performance and style for enhanced productivity on the go.',
-    url: 'https://i.postimg.cc/SsjrbZBR/lg-ultrapc.jpg',
-    category: 'Laptop'
+    imgURL: 'https://i.postimg.cc/SsjrbZBR/lg-ultrapc.jpg',
+    category: 'Laptop',
+    quantity: 37
   },
   {
     id: 4,
     name: 'KODAK PIXPRO Digital Camera',
     price: 185,
     description: "Capture life's moments in stunning detail with this feature-packed and user-friendly camera",
-    url: 'https://i.postimg.cc/sg2K4Nvx/kodak-pixpro-digital-camera.jpg',
-    category: 'Camera'
+    imgURL: 'https://i.postimg.cc/sg2K4Nvx/kodak-pixpro-digital-camera.jpg',
+    category: 'Camera',
+    quantity: 49
   },
   {
     id: 5,
     name: 'Sony PlayStation 4 Slim Gaming Console',
     price: 275,
     description: 'Immerse yourself in gaming with this sleek and powerful console, delivering a premium gaming experience.',
-    url: 'https://i.postimg.cc/SQMKJSdN/sony-playstation4-slim-gaming-console.jpg',
-    category: 'Console'
+    imgURL: 'https://i.postimg.cc/SQMKJSdN/sony-playstation4-slim-gaming-console.jpg',
+    category: 'Console',
+    quantity: 16
   },
   {
     id: 6,
     name: 'Wireless Charger',
     price: 15,
     description: 'Effortlessly power up your devices with cable-free convenience and efficient charging technology.',
-    url: 'https://i.postimg.cc/qvcvJ1kJ/wireless-charger.jpg',
-    category: 'Accessory'
+    imgURL: 'https://i.postimg.cc/qvcvJ1kJ/wireless-charger.jpg',
+    category: 'Accessory',
+    quantity: 25
   },
   {
     id: 7,
     name: 'Motorola Moto G Stylus 5G',
     price: 135,
     description: 'A feature-rich smartphone with 5G capability, a built-in stylus, and impressive camera capabilities.',
-    url: 'https://i.postimg.cc/FzHQvQ9c/motorola-moto-g-stylus-5g.jpg',
-    category: 'Phone'
+    imgURL: 'https://i.postimg.cc/FzHQvQ9c/motorola-moto-g-stylus-5g.jpg',
+    category: 'Phone',
+    quantity: 41
   },
   {
     id: 8,
     name: 'Acer Aspire 3 Slim Laptop',
     price: 349,
     description: 'A sleek and efficient device for everyday computing, combining performance and a stylish design.',
-    url: 'https://i.postimg.cc/250wJpXg/acer-aspire-3-slim-laptop.webp',
-    category: 'Laptop'
+    imgURL: 'https://i.postimg.cc/250wJpXg/acer-aspire-3-slim-laptop.webp',
+    category: 'Laptop',
+    quantity: 21
   },
   {
     id: 9,
     name: 'Canon EOS Rebel Camera',
     price: 479,
     description: 'Unleash your creativity with this versatile and user-friendly DSLR, capturing stunning images with ease.',
-    url: 'https://i.postimg.cc/15svftSp/canon-eos-rebel-camera.jpg',
-    category: 'Camera'
+    imgURL: 'https://i.postimg.cc/15svftSp/canon-eos-rebel-camera.jpg',
+    category: 'Camera',
+    quantity: 59
   },
   {
     id: 10,
     name: 'Nintendo Switch Gaming Console',
     price: 287,
     description: 'A gaming console offering versatility with both handheld and TV modes, providing an immersive gaming experience.',
-    url: 'https://i.postimg.cc/Kv7vD0C6/nintendo-switch-gaming-console.jpg',
-    category: 'Console'
+    imgURL: 'https://i.postimg.cc/Kv7vD0C6/nintendo-switch-gaming-console.jpg',
+    category: 'Console',
+    quantity: 17
   },
   {
     id: 11,
     name: 'Adapter Mount',
     price: 19,
     description: 'Versatile and essential for connecting and securing various devices, ensuring compatibility and convenience.',
-    url: 'https://i.postimg.cc/JzChCfCY/adapter-mount.jpg',
-    category: 'Accessory'
+    imgURL: 'https://i.postimg.cc/JzChCfCY/adapter-mount.jpg',
+    category: 'Accessory',
+    quantity: 27
   },
   {
     id: 12,
     name: 'Google Pixel 7a',
     price: 499,
     description: "A powerful and affordable smartphone, delivering an exceptional camera experience and seamless integration with Google's ecosystem.",
-    url: 'https://i.postimg.cc/mkpW26Dn/google-pixel-7a.jpg',
-    category: 'Phone'
+    imgURL: 'https://i.postimg.cc/mkpW26Dn/google-pixel-7a.jpg',
+    category: 'Phone',
+    quantity: 18
   },
   {
     id: 13,
     name: 'ASUS Vivobook Go Ultra Thin Laptop',
     price: 158.99,
     description: 'Ultra-thin and ultra-portable, this laptop combines sleek design with powerful performance for on-the-go productivity.',
-    url: 'https://i.postimg.cc/FKrpDFGj/asus-vivobook-go-ultra-thin-laptop.webp',
-    category: 'Laptop'
+    imgURL: 'https://i.postimg.cc/FKrpDFGj/asus-vivobook-go-ultra-thin-laptop.webp',
+    category: 'Laptop',
+    quantity: 13
   },
   {
     id: 14,
     name: 'Panasonic LUMIX Mirrorless Camera',
     price: 497,
     description: 'Capture moments with precision and clarity using this advanced mirrorless camera, designed for enthusiasts and professionals alike.',
-    url: 'https://i.postimg.cc/pVD0WjwW/panasonic-lumix-mirrorless-camera.jpg',
-    category: 'Camera'
+    imgURL: 'https://i.postimg.cc/pVD0WjwW/panasonic-lumix-mirrorless-camera.jpg',
+    category: 'Camera',
+    quantity: 11
   },
   {
     id: 15,
     name: 'Valve Steam Deck Handheld Gaming Console',
     price: 57,
     description: 'A handheld gaming console that brings the power of PC gaming to your hands, offering a versatile and immersive gaming experience.',
-    url: 'https://i.postimg.cc/NfvjX3C8/valve-steam-deck-handheld-gaming-console.jpg',
-    category: 'Console'
+    imgURL: 'https://i.postimg.cc/NfvjX3C8/valve-steam-deck-handheld-gaming-console.jpg',
+    category: 'Console',
+    quantity: 19
   },
 ];
