ab7968ba58984c6bbca6d11ccb13be4d415cd146
Tony <nya.tony2010@gmail.com>
2023-12-19 09:52:38 +0100

    feat: add some functions to cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  public cartItemList: any = [];
+  public productList = new BehaviorSubject<any>([]);
 
-  constructor() { }
+  constructor() {}
+
+  addToCart(product: any) {
+    this.cartItemList.push(product);
+    this.productList.next(this.cartItemList);
+    this.getTotalPrice();
+  }
+
+  getProducts() {
+    return this.productList.asObservable();
+  }
+
+  getTotalPrice(): number {
+    let cartTotal = 0;
+    this.cartItemList.map((product: any) => {
+      cartTotal += +product.price;
+    });
+    return cartTotal;
+  }
+
+  removeCartItem(id: any) {
+    this.cartItemList.map((product: any, index: any) => {
+      this.cartItemList.splice(index, 1);
+    });
+    this.productList.next(this.cartItemList);
+  }
 }
