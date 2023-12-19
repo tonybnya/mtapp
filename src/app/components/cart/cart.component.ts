c0155b255960fdc9104be83bfa2f049235255387
Tony <nya.tony2010@gmail.com>
2023-12-19 09:55:02 +0100

    fix: improve and add some modifications to eshop-products-cart components to start handling ordering and add to cart features

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  public products: any = [];
+  public total!: number;
+  constructor(private cartService: CartService) {}
+
   ngOnInit(): void {
+    this.cartService.getProducts().subscribe((res) => {
+      this.products = res;
+      this.total = +this.cartService.getTotalPrice();
+    });
+  }
+
+  removeItem(product: any) {
+    if (confirm('Are you sure you want to remove this product?'))
+      this.cartService.removeCartItem(product);
   }
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiProductsService } from 'src/app/services/api-products.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -10,11 +12,19 @@ export class ProductsComponent implements OnInit {
   page: number = 1;
   public products: any;
 
-  constructor(private api: ApiProductsService) {}
+  constructor(private api: ApiProductsService, private cartService: CartService, private router: Router) {}
 
   ngOnInit(): void {
     this.api.getProducts().subscribe((res) => {
       this.products = res;
+      this.products.forEach((product: any) => {
+        Object.assign(product, { quantity: 1, total: product.price });
+      });
     });
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    this.router.navigateByUrl('cart');
   }
 }
